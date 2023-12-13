dcd8ac83e4bec12de506641243460f9650c5c02e
Bhavnesh Mehra <96747987+Laliforu@users.noreply.github.com>
2023-12-13 15:16:52 +0000

    done

---
+++ basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 20;
+let email = 'bsmehra410@gmail.com'
+var accountPassword = '12345'
+accountCity = "Delhi" // equal to var 
+/*{
+    scope
+    leyt,const(block scope)
+}*/
+// accountId = 26  not allowed assignment on constant variable and not support declaration of same name 
+/* 
+prefer not to use var because of issue in block scope and functional scope 
+ */
+
+email = '2514688455' // let keyword allow assignment but not support declaration of same name 
+console.table([accountId,accountPassword,email,accountCity])
