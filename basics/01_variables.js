cdfb7e8434091c6d5ef1e2618e6030028875bf27
Bhavnesh Mehra <96747987+Laliforu@users.noreply.github.com>
2023-12-15 14:27:51 +0000

    done changes

--- basics/01_variables.js
+++ basics/01_variables.js
@@ -2,6 +2,8 @@ const accountId = 20;
 let email = 'bsmehra410@gmail.com'
 var accountPassword = '12345'
 accountCity = "Delhi" // equal to var 
+
+var state;
 /*{
     scope
     leyt,const(block scope)
@@ -12,4 +14,6 @@ prefer not to use var because of issue in block scope and functional scope
  */
 
 email = '2514688455' // let keyword allow assignment but not support declaration of same name 
-console.table([accountId,accountPassword,email,accountCity])
+console.table([accountId,accountPassword,email,accountCity.state])
+
+/* state = undefined because we didn't declare any value to state */
---
+++ basics/02_datatype.js
@@ -0,0 +1,6 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") we are using node js , not browser 
+
+console.log(3+3)
+console.log(4+5)
