e5b71a94dd91ba5b2db76a7465edea93b1ff77c5
Bhavnesh Mehra <96747987+Laliforu@users.noreply.github.com>
2023-12-31 08:51:03 +0000

    done

--- basics/03_conversion&operations.js
+++ basics/03_conversion&operations.js
@@ -11,10 +11,61 @@
 // "null" = nan
 // undefined = nan
 
-let isLoggedIn = "bhavnesh" 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = "bhavnesh" 
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 
 // 1 => true ; 0=>false
 // ""=>false; "abc" => true
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber, typeof stringNumber);
+
+
+// ************* operations **********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2); // =>4
+// console.log(2-2); // 0
+// console.log(2*2); // 4
+// console.log(2**3); // 8
+// console.log(2/3); // 0.666666666
+// console.log(2%3); // 2
+
+
+let str1 = "hello"
+let str2 = " world"
+let str3 = str1+str2
+// console.log(str3);
+// console.log("1"+2); // 12
+// console.log(1+"2"); // 12
+// console.log("1"+2+2); // 122
+// console.log(1+2+"2"); // 32
+
+// console.log((3+4)*5%3);
+// console.log(+true); // 1
+
+// let num1,num2,num3
+
+// num1,num2,num3 = 3
+
+
+// let counter = 100
+// // counter++  // 101 --> postfix operator
+// ++counter// prefix 
+// console.log(counter); 
+
+let counter = 100
+// let a = counter++ // a = 100 counter = 101  // Postfix
+let a = ++counter
+console.log(`counter = ${counter}  a = ${a}`);
+// counter = 101 a = 101  prefix
+
+
+
+
---
+++ basics/04_comparison.js
@@ -0,0 +1,11 @@
+// console.log(2>1); // = boolean value
+// console.log(2>=1); // = boolean value
+// console.log(2<1); // = boolean value
+// console.log(2==1); // = boolean value
+// console.log(2!=1); // = boolean value  
+
+console.log('02'>1); // true comparison convert string to number
+console.log('2'===2); // false  due to strict check
+console.log('2'==2); // true because == not strict checking
+console.log(null == 0); // false
+console.log(null>=0); // true becasue comparison converts null to interger 0 then equality check whether 0 is equal to 0 or not
