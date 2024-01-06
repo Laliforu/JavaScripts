71e46ee5b814f677ab159a1cf92a7be881e7675e
Bhavnesh Mehra <96747987+Laliforu@users.noreply.github.com>
2024-01-06 09:29:17 +0000

    done

---
+++ basics/datatypes-summary.js
@@ -0,0 +1,82 @@
+// Primitive
+// call by value  (copy of value)
+// 7 category : String, Number , Boolean, Null= empty , undefined= declare but not assigned, Symbol = unique value, BigInt
+/*
+* Premitive Datatypes
+
+    Type               typeof
+
+i)   Number            number
+ii)  String            string
+iii) Boolean           boolean
+iv)  Bigint             bigint
+v)   Symbol             symbol
+vi)  Null               object
+vii) Undefined          undefined
+ */
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+// const bignumber = BigInt(253654455666)
+// console.log(bignumber);
+
+
+
+
+
+
+// Non-Primitive
+// call by reference
+// Array, Objects, Functions
+/*
+        Type               typeof
+i)    Object                object
+ii)   Array                 object
+iii)  Function              function(object)
+*/
+// const a = [1,2,5,3,6]
+// let obj = {
+//     name : 'bhavnesh',
+//     k : 256,
+//     l:[1,2,3,5,{a:[{
+//         a:"name",
+//         b:123
+//     }]}]
+// }
+// // console.log(obj.l[4].a);
+// for(var i = 0; i<obj.l.length;i++){
+//     var e = obj.l[i]
+//     if(typeof e === 'object'){
+//         for(var j = 0; j<e.a.length; j++){
+//             var k = e.a[j]
+//             console.log(k);
+//         }
+//     }
+// }
+
+//  stack(primitve) heap(non-primitive)
+
+let name = "bhavnesh"
+let anotherName = name
+anotherName = "mehra" + name
+// console.log(anotherName);
+var obj = {
+    user:"bsmehra410@gmail.com",
+    a : [1,2,34,{
+        a:"bhavnesh",
+        l:[1,2,3,4,50],
+
+    }],
+}
+// console.log(obj);
+for (let i = 0;i<obj.a.length; i++){
+    let item = obj.a[i]
+    if (instanceof(item,object)){
+        for (let j = 0; j<item.l.length;j++){
+            console.log(j)
+        }
+    }
+}
+
