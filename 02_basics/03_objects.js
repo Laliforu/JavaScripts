678b3550378e63cda56b327422144fb38ddac107
Bhavnesh Mehra <96747987+Laliforu@users.noreply.github.com>
2024-01-11 11:54:51 +0000

    o

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,33 @@
+//  singleton ( apni trh ka unique object ) literal se bnega to si{ngleton nhi bnega
+// Object.create 
+
+
+
+// object literals
+
+const mys1 = Symbol("key1")
+let obj = {
+    name:"bhavnesh",
+    [mys1]:"logged",
+    age:23,
+    location : 'Delhi',
+    email:'bsmehra@gmail.com',
+    isloggedin : false,
+    lastloggedindays: ["monday","wednesday"],
+    "fullname":"bhavnesh mehra",
+}
+obj["fullname"] = "abc"
+// Object.freeze(obj)
+obj["fullname"] = "poikk"
+
+obj.greeting = function(){
+    return `hello ${this.name}`;
+}
+console.log(obj.greeting());
+// console.log(obj['fullname']);
+// console.log(obj[mys1]);
+// console.log(obj);
+
+console.log();
+
+
