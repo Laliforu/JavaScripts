151ad8bb613b44f3c9cc0fa783fd311f84b73d08
Bhavnesh Mehra <96747987+Laliforu@users.noreply.github.com>
2024-01-11 06:20:54 +0000

    k

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,29 @@
+// array
+
+const myArry = [1,2,3,4,5,"true",true]
+const myHeroes = ["shaktiman","nagraaj"]
+const myRR = new Array(1,2,3)
+// console.log(myArry[0]);
+
+// Array Methods*************
+
+// myArry.push(6) // append
+// myArry.push(8) // append
+// myArry.pop() // remove from the last
+// myArry.unshift(20) // add element at the first index [0]
+// myArry.shift()  // remove from the first index
+// console.log(myArry.includes(5));
+// console.log(myArry.indexOf(5));
+
+// const newArr = myArry.join() 
+// console.log(myArry);
+// console.log(newArr); // type string
+
+// console.log("a",myArry);
+// const myn1  = myArry.slice(1,3)
+// console.log(myn1);
+// console.log("b",myArry);
+// const myn2  = myArry.splice(1,3) // manipulate original array
+// console.log("c",myArry);
+// console.log(myn2);
+
