4ba0150cca12f9854ce73cc70d36ac5d7d2ac9ef
Bhavnesh Mehra <96747987+Laliforu@users.noreply.github.com>
2024-01-11 05:16:10 +0000

    math

--- basics/06_maths&num.js
+++ basics/06_maths&num.js
@@ -1,3 +1,28 @@
-const score = 400
-const balance = new Number(400)
-console.log(balance);
+// const score = 400
+// const balance = new Number(600)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const a = 463.856
+// console.log(a.toPrecision(3));
+// const hundreds = 100000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+/* ********************** Maths ******************** */
+
+// console.log(Math);
+// console.log(Math.abs(-4));  // negative to positive
+// console.log(Math.round(4.5)); // round of 4 and half
+// console.log(Math.ceil(4.2)); // always choose top value
+// console.log(Math.floor(4.9)); // always choose lowest value
+// console.log(Math.min(4,3,7,6,8));
+// console.log(Math.max(4,3,7,6,8));
+
+// console.log(Math.floor((Math.random()*10
+// )
+// +1));
+
+const min = 1
+const max = 6
+console.log(Math.floor(Math.random()* (max - min + 1)) + min)
